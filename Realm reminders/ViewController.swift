e59f2151d0cf4e2728a8839925c18253af6dafdb
Tymon Goleba Frygies <tymon.gf@hotmail.com>
2020-10-06 19:07:04 +0200

    Initial Commit

---
+++ Realm reminders/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//	Realm reminders : ViewController.swift by Tymek on 06/10/2020 19:06.
+//	Copyright ©Tymek 2020. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
